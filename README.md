089a9291e0f944a63985d1c85043974b07d642b5
Ricardo Lebrón <rlebron88@gmail.com>
2018-03-24 23:12:40 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Clownium 🤡
+
+## How to compile
+
+```bash
+
+git clone https://github.com/rlebron88/Clownium.git
+
+cd Clownium
+
+npm install
+
+truffle compile
+
+```
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-let OmeOme1MToken = artifacts.require("OmeOme1MToken");
+let Clownium = artifacts.require("Clownium");
 
 module.exports = function (deployer) {
-	deployer.deploy(OmeOme1MToken, {from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", gas: 5000000});
+	deployer.deploy(Clownium, {from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", gas: 5000000});
 };
